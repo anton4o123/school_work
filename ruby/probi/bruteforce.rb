7e12d595a5fc5cfeb71bca3103fed0e6b7576ba3
anton4o123 <anton4o123@abv.bg>
2013-11-22 21:29:12 +0200

    That need a fix

--- ruby/probi/bruteforce.rb
+++ ruby/probi/bruteforce.rb
@@ -1,16 +1,30 @@
 require 'time'
 def bruteforce pass_expect,pass="",len=1
 	i=0
-	while i<26
-		if pass_expect==pass+(i+97).chr
-			puts pass+(i+97).chr
+	while i<62
+		if i<26
+			c=(i+97).chr
+		elsif i<52
+			c=(i+39).chr
+		else
+			c=(i-4).chr
+		end
+		if pass_expect==pass+c
+			puts pass+c
+			i=101
 			break
 		end
-		if (pass+(i+97).chr).length<len
-			bruteforce pass_expect,pass+(i+97).chr,len
+		if (pass+c).length<len
+			bruteforce pass_expect,pass+c,len
+			puts 'ho'
+			puts i
+		end
+		if i>100
+			break
 		end
 		i+=1
 	end
+	return i
 end
 
 d=Time.now
