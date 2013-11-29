cc2d089e7c5d64993f42d407d54b0b4b7e652860
anton4o123 <anton4o123@abv.bg>
2013-11-29 19:32:16 +0200

    Hello java world

---
+++ JAVA/hello.java
@@ -0,0 +1,13 @@
+public class hello {
+String hell;
+
+void shout() {
+hell="Hello Java World!";
+System.out.println(hell);
+}
+
+public static void main(String[] args) {
+hello p=new hello();
+p.shout();
+}
+}
