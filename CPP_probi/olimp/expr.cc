e82d94f2f58967a0d1e5b112d6d4b49e81534325
anton4o123 <anton4o123@abv.bg>
2013-12-14 19:59:18 +0200

    Now works for every values

--- CPP_probi/olimp/expr.cc
+++ CPP_probi/olimp/expr.cc
@@ -4,36 +4,39 @@ using namespace std;
 
 int main() {
 	string brackets;
-	int flag=1,i=0,opened=0;
-	char control='(';
+	int flag=1,i=0,opened=0,big_opened=0;
+	char control='(',big_control='[';
 	
 	cin >> brackets;
-	while(i<brackets.length()) {
-		if(brackets[i]!='(' && brackets[i]!=')' && brackets[i]!='[' && brackets[i]!=']') {
-			flag=0;
-			break;
-		}
-		i++;
-	}
 	
 	if(flag==1) {
 		i=0;
 		while(i<brackets.length()) {
-			if(brackets[i]==control) {
-				if(control=='(') {
-					opened++;
-					control=')';
+			if(brackets[i]==big_control) {
+				if(big_control=='[') {
+					big_opened++;
+					big_control=']';
 				} else {
-					opened--;
-					control='(';
+					big_opened--;
+					big_control='[';
 				}
 			} else {
-				opened=1;
-				break;
+				if(brackets[i]==control) {
+					if(control=='(') {
+						opened++;
+						control=')';
+					} else {
+						opened--;
+						control='(';
+					}
+				} else {
+					opened=1;
+					break;
+				}
 			}
 			i++;
 		}
-		if(opened!=0) {
+		if(opened!=0 || big_opened!=0) {
 			flag=0;
 		}
 	}
