e8695e09541847465ab044b2f52c8f47cfdafbc2
anton4o123 <student11a@ws01.(none)>
2014-02-20 17:45:13 +0200

    Need to be finished

---
+++ OS_11_klas/threads2/threads.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 10000
+
+int cmp(void* n1, void* n2) {
+	return (*(int*)n1)-(*(int*)n2);
+}
+
+void* merge_sort(void* arr) {
+	qsort((int*)arr, SIZE/2, sizeof(int), (void*)cmp);
+	return (void*)0;
+}
+
+int main() {
+	srand((unsigned)time(NULL));
+	int arr[SIZE];
+	int i;
+	for(i=0; i<SIZE; ++i) {
+		arr[i]=rand()/(RAND_MAX/(SIZE*10)+1);
+	}
+	
+	pthread_t thread1;
+	pthread_t thread2;
+	pthread_create(&thread1, NULL, merge_sort, (void*)arr);
+	pthread_create(&thread2, NULL, merge_sort, (void*)&arr[SIZE/2]);
+
+	for(i=0; i<SIZE; ++i) {
+		printf("%d\n",arr[i]);
+	}
+	pthread_exit(NULL);
+	return 0;
+}
