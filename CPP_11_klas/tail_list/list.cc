295c0453ca7712db3708560d3a765fb5fdfacb37
anton4o123 <student11a@ws01.(none)>
2014-03-06 14:11:03 +0200

    pop_back

--- CPP_11_klas/tail_list/list.cc
+++ CPP_11_klas/tail_list/list.cc
@@ -14,9 +14,7 @@ class List {
 	Node* tail_;
 public:
 	List()
-	: head_(0), tail_(0) {
-	//	head_->next_=tail_;
-	}
+	: head_(0), tail_(0) {}
 	
 	bool empty() {
 		return head_==0;
@@ -67,12 +65,55 @@ public:
 		return value;
 	}
 	
-/*	T pop_back() {
+	T pop_back() {
 		Node* del_node=tail_;
+		Node* help_node=head_;
 		T value=tail_->data_;
+		iterator it(head_);
 		
-		tail_
-	}*/
+		while(true) {
+			help_node=it.current_;
+			if(help_node->next_==tail_->next_) {
+				break;
+			}
+			it++;
+		}
+		tail_=help_node;
+		tail_->next_=help_node;
+		delete del_node;
+		return value;
+	}
+	
+	class iterator {
+		friend class List;
+		Node* current_;
+		iterator(Node* current)
+		: current_(current) {}
+	public:
+		iterator& operator++() {
+			current_=current_->next_;
+			return *this;
+		}
+		
+		iterator operator++(int) {
+			iterator res=*this;
+			
+			current_=current_->next_;
+			return res;
+		}
+		
+		int& operator*() {
+			return current_->data_;
+		}
+		
+		bool operator==(const iterator& other) const {
+			return current_==other.current_;
+		}
+		
+		bool operator!=(const iterator& other) const {
+			return !operator==(other);
+		}
+	};
 };
 
 int main() {
@@ -81,11 +122,14 @@ int main() {
 	cout << l1.empty() << endl;
 	l1.push_back(6);
 	l1.push_back(5);
+	l1.push_front(7);
+	l1.push_back(4);
 	cout << l1.empty() << endl;
 	cout << l1.front() << endl;
 	cout << l1.back() << endl;
 	cout << l1.pop_front() << endl;
 	cout << l1.pop_front() << endl;
+	cout << l1.pop_back() << endl;
 	cout << l1.empty() << endl;
 
 	return 0;
