176a2826559b377b63bd0b4de02c16d505487194
anton4o123 <student11a@ws01.(none)>
2014-04-15 17:26:24 +0300

    Second part

---
+++ CPP_11_klas/streams/except.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.exceptions(ios_base::badbit | ios_base::failbit | ios_base::eofbit);
+	
+	int val;
+	
+	try {
+		while(true) {
+			cin >> val;
+			cout << "val=" << val << endl;
+		}
+	} catch(ios_base::failure& ex) {
+		cout << "io exception caught..." << endl;
+	}
+	
+	return 0;
+}
---
+++ CPP_11_klas/streams/get.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch;
+	
+	while(cin) {
+	//	cin >> ch;
+		cin.get(ch);
+		if(!cin) {
+			break;
+		}
+		cout << "ch: " << ch << endl;
+	}
+	
+	return 0;
+}
